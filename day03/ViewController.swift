ddc44e84a1e4ea94f06e017037c813c77535429c
ProtossArtanis <steinsgate@mac24.local>
2016-03-17 10:20:37 +0800

    Initial Commit

---
+++ day03/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  day03
+//
+//  Created by SteinsGate on 16/3/17.
+//  Copyright © 2016年 SteinsGate. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
